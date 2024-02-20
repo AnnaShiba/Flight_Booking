d81684c761dac6fb6576e67b8dad6a7129412a0f
Anna Shiba <102438674+AnnaShiba@users.noreply.github.com>
2024-02-19 19:34:00 -0500

    Seed test data

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -7,5 +7,41 @@ namespace COMP2139_Assignment.Data {
 
         public DbSet<Hotel> Hotels { get; set; }
         public DbSet<Booking> Bookings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel {
+                    HotelId = 1,
+                    Name = "Villa Coco",
+                    Amentities = "All-inclusive resort with outdoor pool and beach access",
+                    Location = "Varadero",
+                    Price = 299.99,
+                    Reviews = 4.5
+                },
+                new Hotel {
+                    HotelId = 2,
+                    Name = "Hotel Varadero",
+                    Amentities = "All-inclusive resort with beach access",
+                    Location = "Varadero",
+                    Price = 199.99,
+                    Reviews = 3.7
+                }
+            );
+
+            modelBuilder.Entity<Booking>().HasData(
+                new Booking {
+                    BookingId = 1,
+                    StartDate = new DateTime(2024, 05, 13),
+                    EndDate = new DateTime(2024, 05, 27),
+                    HotelId = 1
+                },
+                new Booking {
+                    BookingId = 2,
+                    StartDate = new DateTime(2024, 04, 13),
+                    EndDate = new DateTime(2024, 04, 27),
+                    HotelId = 2
+                }
+            );
+        }
     }
 }
---
+++ Migrations/20240220003158_SeedInitialData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace COMP2139_Labs.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "HotelId", "Amentities", "Image", "Location", "Name", "Price", "Reviews" },
+                values: new object[,]
+                {
+                    { 1, "All-inclusive resort with outdoor pool and beach access", "", "Varadero", "Villa Coco", 299.99000000000001, 4.5 },
+                    { 2, "All-inclusive resort with beach access", "", "Varadero", "Hotel Varadero", 199.99000000000001, 3.7000000000000002 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Bookings",
+                columns: new[] { "BookingId", "EndDate", "HotelId", "StartDate" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 5, 27, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, new DateTime(2024, 5, 13, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, new DateTime(2024, 4, 27, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, new DateTime(2024, 4, 13, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Bookings",
+                keyColumn: "BookingId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Bookings",
+                keyColumn: "BookingId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "HotelId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "HotelId",
+                keyValue: 2);
+        }
+    }
+}
